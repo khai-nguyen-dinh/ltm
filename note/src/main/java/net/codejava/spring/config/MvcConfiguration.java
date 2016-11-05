5ea0792af3d740ca707b7060b72aea8c3dd9091b
khainguyen <khainguyenptiter@gmail.com>
2016-11-05 17:13:23 +0700

    updtae

--- note/src/main/java/net/codejava/spring/config/MvcConfiguration.java
+++ note/src/main/java/net/codejava/spring/config/MvcConfiguration.java
@@ -38,7 +38,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://mysql:3306/note");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/note");
         dataSource.setUsername("root");
         dataSource.setPassword("nguyendinhkhai");
 
